c73b5fbff2bdcb0a1d9bf404d29a44037e6b908e
Ibanesspadin <ibanesspadin@gmail.com>
2026-01-07 01:36:04 -0300

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,17 +1,11 @@
 import streamlit as st
-from google.oauth2.service_account import Credentials
 import gspread
+from google.oauth2.service_account import Credentials
 
-scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-
-# Pega o JSON do Secret do Streamlit
-service_account_info = st.secrets["gcp_service_account"]
-
-# Cria as credenciais
-creds = Credentials.from_service_account_info(service_account_info, scopes=scopes)
-
-# Conecta com o Google Sheets
-gc = gspread.authorize(creds)
-
-def connect_sheets():
-    return gc
+@st.cache_resource
+def connect_sheet():
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=["https://www.googleapis.com/auth/spreadsheets"]
+    )
+    return gspread.authorize(creds)
